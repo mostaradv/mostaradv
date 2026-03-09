3a3dac76c189a861bd09e6bab07fdc9f8e8f86fc
MostarADV <Mostaradv@gmail.com>
2026-03-09 10:55:19 +0000

    Add favicon and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,13 @@ export const metadata = {
   verification: {
     google: "usxWiKVgrc64nySOgRbBYl3vK0x3un5vMlyxPIdBPMY",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
